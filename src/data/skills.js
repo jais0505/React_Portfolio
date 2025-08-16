774c4ab78bdee9ff9294595122b3837e75563d5f
Jais Jose <jaisjose44@gmail.com>
2025-08-16 09:32:21 +0530

    React sample portfolio inital commit

---
+++ src/data/skills.js
@@ -0,0 +1,40 @@
+import html from "../../assets/skills/html.png";
+import css from "../../assets/skills/css.png";
+import react from "../../assets/skills/react.png";
+import node from "../../assets/skills/node.png";
+import graphql from "../../assets/skills/graphql.png";
+import mongodb from "../../assets/skills/mongodb.png";
+import figma from "../../assets/skills/figma.png";
+
+const skills = [
+  {
+    "title": "HTML",
+    "imageSrc": html
+  },
+  {
+    "title": "CSS",
+    "imageSrc": css
+  },
+  {
+    "title": "React",
+    "imageSrc": react
+  },
+  {
+    "title": "Node",
+    "imageSrc": node
+  },
+  {
+    "title": "GraphQL",
+    "imageSrc": graphql
+  },
+  {
+    "title": "MongoDB",
+    "imageSrc": mongodb
+  },
+  {
+    "title": "Figma",
+    "imageSrc": figma
+  }
+];
+
+export default skills;
